3c5ed3982e2b62b5e3da6168baa2233d2a032fd9
“liyibass” <liyibass@mirrormedia.mg>
2020-09-15 18:56:08 +0800

    add single author method.

--- index.js
+++ index.js
@@ -64,6 +64,18 @@ const RootQueryType = new GraphQLObjectType({
       resolve: () => books,
     },
 
+    author: {
+      type: AuthorType,
+      description: "List single author",
+      args: {
+        id: {
+          type: GraphQLInt,
+        },
+      },
+      resolve: (parent, args) =>
+        authors.find((author) => author.id === args.id),
+    },
+
     authors: {
       type: new GraphQLList(AuthorType),
       description: "List of All Authors",
