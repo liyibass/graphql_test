1a687513acea7ea26848a5c6a32e9e0f3b13e583
“liyibass” <liyibass@mirrormedia.mg>
2020-09-15 18:08:30 +0800

    Basic graphQL structure.

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require("graphql");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const { authors, books } = require("./data/data");
+
+const AuthorType = new GraphQLObjectType({
+  name: "Auther",
+  description: "This represents authors",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+  }),
+});
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "This represents a book written by an author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: (book) => authors.find((author) => author.id === book.id),
+    },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "Query",
+  description: "Root Query",
+  fields: () => ({
+    books: {
+      type: new GraphQLList(BookType),
+      description: "List of All books",
+      resolve: () => books,
+    },
+
+    authors: {
+      type: new GraphQLList(AuthorType),
+      description: "List of All Authors",
+      resolve: () => authors,
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server has started on port ${PORT}`);
+});
